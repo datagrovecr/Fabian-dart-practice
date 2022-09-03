84fae0dfa9912920fcc27af7bb29b8ef912b88e6
fabianvalverde <110683318+fabianvalverde@users.noreply.github.com>
2022-09-03 01:09:33 -0600

    Code Wars problem file solution
    You can see and check the problem here:
    
    https://www.codewars.com/kata/5287e858c6b5a9678200083c/train/typescript
    
    Run the code typing in the terminal: "node NarcicistNumber.js" and being in the right directory (cd ./TSPractice)

---
+++ Main.dart
@@ -0,0 +1,28 @@
+import './sampleCode.dart';
+
+void main() {
+  var x = "Just an x";
+  var y = " and here you have some operators examples";
+  printFunction("Just a simple Hello World");
+  var num = 4;
+  printFunction(x + y);
+  assignmentOperators(num);
+  convertingTask();
+  makingDecisions(23);
+  booleans(true);
+  booleanExpressions();
+  moduloOperator(4);
+  switchAssignment(4);
+  whileLoop();
+  doWhileLoop();
+  forLoop();
+  functionParameters(name: "Fabian", status: true);
+  functionParameters2("Pedro", 10324);
+  print(functionReturn('Hola buenas'));
+  print(functionReturn2("Hello ", ":)"));
+  examiningString("Heulouo my hel frend l");
+  modifyingStrings();
+  lists();
+  //print(isograms("rgdgs"));
+  print(isIsogram("qwerqty"));
+}
